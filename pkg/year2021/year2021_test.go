1d67037ee6c40bac536ecc16314c698489e9af7b
Kristoffer Moberg Christensen <KristofferMoberg.Christensen@skatteetaten.no>
2023-11-29 20:28:14 +0100

    feat: day04

--- pkg/year2021/year2021_test.go
+++ pkg/year2021/year2021_test.go
@@ -75,3 +75,20 @@ func Benchmark2021Day04(b *testing.B) {
 		}
 	})
 }
+func Benchmark2021Day05(b *testing.B) {
+	Init()
+	input := aoc.TestInput(2021, 5)
+	p := aoc.NewPuzzle(2021, 5)
+	b.Run("PartA", func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			p.PartA(input)
+		}
+	})
+	b.Run("PartB", func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			p.PartB(input)
+		}
+	})
+}
